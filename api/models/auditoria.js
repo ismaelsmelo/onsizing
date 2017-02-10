8b04bb909658eff633d1ea8523ae05b2c4103b04
Ismael da Silva de Melo <ismael_melo@sicredi.com.br>
2017-02-09 22:54:48 -0200

    20170209 - atualiza novamente

--- api/models/auditoria.js
+++ api/models/auditoria.js
@@ -4,11 +4,11 @@ const mongoose = restful.mongoose
 const Auditoria_Schema = new mongoose.Schema( {
   id_usuario: { type: String, required: [ true, 'Informe o código do usuário.' ] }, //id do usuário logado
   ts_acesso: { type: String }, //data e hora do acesso
-  id_funcionalidade {  type: String }, //funcionalidade da contagem
-  id_pagina { type: String }, //caso tenha tela, informar qual
-  id_servico { type: String }, //caso seja serviço
-  ds_imagemAntes { type: String }, //caso necessário, informar
-  ds_imagemDepois { type: String } //caso necessário, informar
+  id_funcionalidade: { type: String }, //funcionalidade da contagem
+  id_pagina: { type: String }, //caso tenha tela, informar qual
+  id_servico: { type: String }, //caso seja serviço
+  ds_imagemAntes: { type: String }, //caso necessário, informar
+  ds_imagemDepois: { type: String } //caso necessário, informar
 })
 
 module.exports = restful.model('Auditoria', Auditoria_Schema)
