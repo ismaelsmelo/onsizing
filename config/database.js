9003fd2cc7a3654089a103f624581324362c9eaf
Ismael da Silva de Melo <ismael_melo@sicredi.com.br>
2017-02-14 22:27:59 -0200

    conectar MongoLab 2

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 //module.exports = mongoose.connect('mongodb://localhost:27017/db_base')
-module.exports = mongoose.connect('mongodb://ismael.melo:eufrates@ds113938.mlab.com:13938/onsizing_base')
+//module.exports = mongoose.connect('mongodb://ismael.melo:eufrates@ds113938.mlab.com:13938/onsizing_base')
+module.exports = mongoose.connect('mongodb://ismael.melo:eufrates@ds149489.mlab.com:49489/heroku_9bj1csjb')
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
